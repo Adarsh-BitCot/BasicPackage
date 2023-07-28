d651754fc05851a7ed7fc1949cde2a36e428eab6
Adarsh Sharma <adarshsharma@bitcot.com>
2023-07-28 15:49:12 +0530

    main

---
+++ README.md
@@ -0,0 +1,3 @@
+# BasicPackage2
+
+A description of this package.
---
+++ Tests/BasicPackage2Tests/BasicPackage2Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BasicPackage2
+
+final class BasicPackage2Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BasicPackage2().text, "Hello, World!")
+    }
+}
