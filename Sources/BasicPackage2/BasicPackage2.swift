641f6412e480ebb72627b4c1036f1f0237e2bac5
Adarsh Sharma <adarshsharma@bitcot.com>
2023-07-29 09:42:32 +0530

    api

--- Sources/BasicPackage2/BasicPackage2.swift
+++ Sources/BasicPackage2/BasicPackage2.swift
@@ -12,42 +12,49 @@ public struct BasicPackage2 {
     }
 }
 
-public func callAPI(urlString: String, httpMethod: HTTPRequest.HTTPMethod, params: [String:Any] = [:], body: [String:Any]? = .none) {
-    let client: HTTPClient = NetworkClient()
-    guard let url = URL(string: urlString) else { return }
+//public func callAPI(urlString: String,
+//                    httpMethod: HTTPRequest.HTTPMethod,
+//                    params: [String:Any] = [:],
+//                    body: [String:Any]? = .none,
+//                    headers: [String:String] = [:]) {
+//    let client: HTTPClient = NetworkClient()
+//    guard let url = URL(string: urlString) else { return }
+//
+//    let request = HTTPRequest(
+//        url: url,
+//        httpMethod: httpMethod,
+//        params: params,
+//        body: body,
+//        customHeaders: headers
+//    )
+//
+//    client.request(request, type: BaseModel<User>.self) { result in
+//        switch result {
+//        case .success(let response):
+//            completion(.success(response))
+//        case .failure(let error):
+//            completion(.failure(NetworkError.network(error)))
+//        }
+//    }
+//}
+
+
+public func getAPIData(url: String) -> Data{
+    var apiData: Data?
     
-    let request = HTTPRequest(
-        url: url,
-        httpMethod: httpMethod,
-        params: params,
-        body: body,
-        customHeaders: headers
-    )
-    
-    client.request(request, type: BaseModel<User>.self) { result in
-        switch result {
-        case .success(let response):
-            completion(.success(response))
-        case .failure(let error):
-            completion(.failure(NetworkError.network(error)))
+    AF.request(url).response { response in
+        if let data = response.data{
+//            do{
+//                let decoded = try JSONDecoder().decode(model.self, from: data)
+////                self.dataSet.append((decoded.data?.first)!)
+//            }catch{
+//                print(error.localizedDescription)
+//            }
+            apiData = data
         }
     }
+    return apiData ?? Data()
 }
-
-//@available(iOS 13.0.0, *)
-//public func callAPI(url: String) -> some View{
-////    AF.request(url).response { response in
-////        if let data = response.data{
-////            do{
-////                let decoded = try JSONDecoder().decode(model, from: data)
-//////                self.dataSet.append((decoded.data?.first)!)
-////            }catch{
-////                print(error.localizedDescription)
-////            }
-////        }
-////    }
-//    return Text("API Called")
-//}
 
 public func uploadImage(image: UIImage, baseUrl: String, closer: @escaping (Bool) -> Void){
     var headers:HTTPHeaders{
